52a7ec1dcd91b4f648b62e00d590cddcea624f7b
Raymond Siudak <r.siudak@ultimaker.com>
2020-05-28 14:02:46 +0200

    Add crosscompiling with sysroot including our own compiled Mesa libraries
    - Wrap everything in the usual build_for_ultimaker.sh dockerized interface
    - Add Qt base patches and apply them dynamically on submodule update
    - Fix submodule paths
    - Fix install dir problem
    - Use host machine installed Qemu
    - Set makeflags to use CORES-1 not to hog up you dev machine
    - Implement proper shell script template for build_sysroot.sh
    - Overal cleaned up the scripts
    - Set QT build to release, the package was to big to install
    - Add .gitignore for sysroot and build output
    - Reinstate the docker env check to be warned early in case of problems
    
    Contributes to CES-149 and CES-147
    
    Signed-off-by: Raymond Siudak <r.siudak@ultimaker.com>

---
+++ build_for_ultimaker.sh
@@ -0,0 +1,131 @@
+#!/bin/sh
+#
+# SPDX-License-Identifier: AGPL-3.0+
+#
+# Copyright (C) 2019 Ultimaker B.V.
+#
+
+set -eu
+
+LOCAL_REGISTRY_IMAGE="qt5-ultimaker"
+
+ARCH="${ARCH:-armhf}"
+SRC_DIR="$(pwd)"
+PREFIX="/usr"
+RELEASE_VERSION="${RELEASE_VERSION:-5.12.3}"
+DOCKER_WORK_DIR="/docker_workdir"
+BUILD_DIR_TEMPLATE="_build"
+BUILD_DIR="${BUILD_DIR_TEMPLATE}"
+
+
+run_env_check="yes"
+
+update_docker_image()
+{
+    echo "Building local Docker build environment."
+    docker build ./docker_env -t "${LOCAL_REGISTRY_IMAGE}"
+}
+
+run_in_docker()
+{
+    docker run \
+        --privileged \
+        --cap-add=ALL \
+        --security-opt seccomp:unconfined \
+        --rm \
+        -it \
+        -u "$(id -u)" \
+        -e "BUILD_DIR=${DOCKER_WORK_DIR}/${BUILD_DIR}" \
+        -e "ARCH=${ARCH}" \
+        -e "PREFIX=${PREFIX}" \
+        -e "RELEASE_VERSION=${RELEASE_VERSION}" \
+        -e "MAKEFLAGS=-j$(($(getconf _NPROCESSORS_ONLN) - 1))" \
+        -v "${SRC_DIR}:${DOCKER_WORK_DIR}" \
+        -w "${DOCKER_WORK_DIR}" \
+        "${LOCAL_REGISTRY_IMAGE}" \
+        "${@}"
+}
+
+update_modules()
+{
+    git submodule update --init --recursive --depth 1
+    cd "${SRC_DIR}/qtbase"
+    for patch in "${SRC_DIR}/patches/qtbase/"*.patch; do
+        if git apply --check "${patch}" > /dev/null 2>&1; then
+            git apply "${patch}"
+        fi
+    done
+}
+
+env_check()
+{
+    run_in_docker "./docker_env/buildenv_check.sh"
+}
+
+run_build()
+{
+    run_in_docker "./build.sh" "${@}"
+}
+
+deliver_pkg()git
+{
+    cp "${BUILD_DIR}/"*"deb" "./"
+}
+
+run_tests()
+{
+    echo "There are no tests available for this repository."
+}
+
+usage()
+{
+    echo "Usage: ${0} [OPTIONS]"
+    echo "  -c   Clean the workspace"
+    echo "  -C   Skip run of build environment checks"
+    echo "  -h   Print usage"
+    echo
+    echo "Other options will be passed on to build.sh"
+    echo "Run './build.sh -h' for more information."
+}
+
+while getopts ":ch" options; do
+    case "${options}" in
+    c)
+        run_build "${@}"
+        exit 0
+        ;;
+    C)
+        run_env_check="no"
+        ;;
+    h)
+        usage
+        exit 0
+        ;;
+    :)
+        echo "Option -${OPTARG} requires an argument."
+        exit 1
+        ;;
+    ?)
+        echo "Invalid option: -${OPTARG}"
+        exit 1
+        ;;
+    esac
+done
+shift "$((OPTIND - 1))"
+
+if ! command -V docker; then
+    echo "Docker not found, docker-less builds are not supported."
+    exit 1
+fi
+
+update_docker_image
+
+if [ "${run_env_check}" = "yes" ]; then
+    env_check
+fi
+
+update_modules
+run_build "${@}"
+deliver_pkg
+
+exit 0
