4ecd7b8303107d01cbd38b88956f477f4edb938c
Robin den Hertog <r.denhertog@ultimaker.com>
2020-11-03 11:32:09 +0100

    MISP-1132: switching over to submodules instead of patches applied by a script
    Signed-off-by: Robin den Hertog <r.denhertog@ultimaker.com>

--- build_for_ultimaker.sh
+++ build_for_ultimaker.sh
@@ -50,17 +50,6 @@ run_in_docker()
 update_modules()
 {
     git submodule update --init --recursive --depth 1
-    cd "${SRC_DIR}/qtbase"
-    for patch in "${SRC_DIR}/patches/qtbase/"*.patch; do
-        if git apply --check "${patch}" > /dev/null 2>&1; then
-            git apply "${patch}"
-        fi
-    done
-    for patch in "${SRC_DIR}/patches/qtdeclerative/"*.patch; do
-        if git apply --check "${patch}" > /dev/null 2>&1; then
-            git apply "${patch}"
-        fi
-    done
 }
 
 run_shellcheck()
