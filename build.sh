3fe64de25356408dba251c9dc1ff2e6d2072bc1d
bryanfoley <bryan.foley@ultimaker.com>
2024-04-16 14:44:50 +0200

    WIP: remove unrecognised config options

--- build.sh
+++ build.sh
@@ -56,7 +56,6 @@ build()
         -kms \
         -no-directfb \
         -nomake tests \
-        -nomake tools \
         -nomake examples \
         -no-cups \
         -no-sql-db2 \
@@ -66,8 +65,6 @@ build()
         -no-sql-odbc \
         -no-sql-psql \
         -no-sql-sqlite \
-        -no-sql-sqlite2 \
-        -no-sql-tds \
         -skip qtconnectivity \
         -skip qtdoc \
         -skip qtlocation \
